305fc2eb6318dd99aae5820da41d66304d19ecdc
Viktor Dehtiarov <viktor.beet.root@gmail.com>
2023-11-14 03:50:51 +0200

    Updates

--- assets/Home-e1dbc033.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-0940e422.js";function o(){return e.jsx("h1",{className:"text-5xl",children:"Home"})}export{o as Component,o as default};
---
+++ assets/Tv-6d31d811.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-6987a6d0.js";function s(){return t.jsx("h1",{className:"text-5xl",children:"Tv"})}export{s as Component,s as default};
